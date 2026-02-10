32244180b04a5bcfc54b2169ed223dc7ce1fa45c
Aperez <andres.perezcorona81@gmail.com>
2026-02-10 11:48:29 -0600

    refactor(plan-template): adjust grid layout for responsive design in PlanView

--- web/src/views/plan/PlanView.vue
+++ web/src/views/plan/PlanView.vue
@@ -414,8 +414,12 @@ watch(pendingDelete, (pd) => {
 
   &__grid {
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: 1fr;
     gap: 12px;
+
+    @media (min-width: $breakpoint-tablet-min-width) {
+      grid-template-columns: repeat(2, 1fr);
+    }
   }
 
   &__card {
